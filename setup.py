9a3e426583ea3e0bc912cd17096f4cd74344cbd5
Peter Pfeufer <info@ppfeufer.de>
2021-11-30 08:14:20 +0100

    Minimum requirements updated
      - Python 3.7
      - Alliance Auth v2.9.3

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ project_issues_url = f"{project_git_url}/issues"
 project_changelog_url = f"{project_git_url}/blob/master/CHANGELOG.md"
 project_homepage_url = project_git_url
 project_install_requirements = [
-    "allianceauth>=2.8.8",
+    "allianceauth>=2.9.3",
     "allianceauth-discordbot",
     "feedparser",
     "discord.py",
 ]
-project_python_requires = "~=3.6"
+project_python_requires = "~=3.7"
 project_classifiers = [
     "Environment :: Web Environment",
     "Framework :: Django",
@@ -42,10 +42,10 @@ project_classifiers = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Topic :: Internet :: WWW/HTTP",
     "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
 ]
