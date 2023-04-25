3fbe7ae7eba01204f7499afcf8218788dc34916a
Peter Pfeufer <info@ppfeufer.de>
2023-04-25 22:46:49 +0200

    [FIX] Package name

--- aa_rss_to_discord/__init__.py
+++ aa_rss_to_discord/__init__.py
@@ -5,7 +5,7 @@ App init
 # Standard Library
 from importlib import metadata
 
-__version__ = metadata.version("aa-fleetfinder")
+__version__ = metadata.version("aa-rss-to-discord")
 __title__ = "RSS to Discord"
 
 del metadata
