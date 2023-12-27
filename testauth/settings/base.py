d5dd33a493fd9b057446f5a052ecdbaa1595ba97
Peter Pfeufer <info@ppfeufer.de>
2023-12-27 18:12:12 +0100

    [CHANGE] Prepare for Alliance Auth v4

--- testauth/settings/base.py
+++ testauth/settings/base.py
@@ -15,7 +15,6 @@ from celery.schedules import crontab
 # Django
 from django.contrib import messages
 
-# pylint: disable=line-too-long
 INSTALLED_APPS = [
     "allianceauth",  # needs to be on top of this list to support favicons in Django admin (see https://gitlab.com/allianceauth/allianceauth/-/issues/1301)
     "django.contrib.admin",
@@ -27,8 +26,10 @@ INSTALLED_APPS = [
     "django.contrib.humanize",
     "django_celery_beat",
     "bootstrapform",
+    "django_bootstrap5",  # https://github.com/zostera/django-bootstrap5
     "sortedm2m",
     "esi",
+    "allianceauth.framework",
     "allianceauth.authentication",
     "allianceauth.services",
     "allianceauth.eveonline",
@@ -36,6 +37,11 @@ INSTALLED_APPS = [
     "allianceauth.notifications",
     "allianceauth.thirdparty.navhelper",
     "allianceauth.analytics",
+    "allianceauth.menu",
+    "allianceauth.theme",
+    "allianceauth.theme.darkly",
+    "allianceauth.theme.flatly",
+    "allianceauth.theme.materia",
 ]
 
 SECRET_KEY = "wow I'm a really bad default secret key"
@@ -71,6 +77,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 MIDDLEWARE = [
+    "allianceauth.menu.middleware.MenuSyncMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "allianceauth.authentication.middleware.UserSettingsMiddleware",
@@ -80,7 +87,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allianceauth.analytics.middleware.AnalyticsMiddleware",
 ]
 
 ROOT_URLCONF = "allianceauth.urls"
@@ -189,6 +195,9 @@ DATABASES = {
 }
 
 SITE_NAME = "Alliance Auth"
+
+DEFAULT_THEME = "allianceauth.theme.flatly.auth_hooks.FlatlyThemeHook"
+DEFAULT_THEME_DARK = "allianceauth.theme.darkly.auth_hooks.DarklyThemeHook"  # Legacy AAv3 user.profile.night_mode=1
 
 LOGIN_URL = "auth_login_user"  # view that handles login logic
 
